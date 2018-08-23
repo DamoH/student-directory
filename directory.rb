f8c493ca650e67603eaa8b45e435e512f7a4dcb0
Damion Hardy <damionhardy@hotmail.com>
2018-08-23 13:26:45 +0100

    step 8 exercises 1-4 added

--- directory.rb
+++ directory.rb
@@ -22,9 +22,22 @@ def print_header
   puts "-------------"
 end
 
+=begin
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+=end
+
+# Step 8; exercise  (print method using while loop)
+def print(students)
+  count = 1
+  while count < students.count do
+    students.each_with_index do |student, index|
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+    count += 1
   end
 end
 
@@ -32,8 +45,29 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def print_begin_letter(students, letter)
+  students.each do |student|
+    if student[:name].start_with?("#{letter}")
+      puts "#{student[:name]} starts with '#{letter}'"
+    end
+  end
+end
+
+def print_shorter_than_12_chars(students)
+  puts "Student(s) names that are less that 12 characters long:"
+  students.each do |student|
+    if student[:name].count("a-zA-z") < 12
+      puts student[:name]
+    end
+  end
+end
+
 students = input_students
 #nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
+# Step 8; exercise 2
+print_begin_letter(students, "D")
+# Step 8; exercise 3
+print_shorter_than_12_chars(students)
