c62dcbb34b6fa9317590710e8695e860d85df75b
Damion Hardy <damionhardy@hotmail.com>
2018-08-28 09:21:16 +0100

    commiting changes after more exercises 1&2 of step 14

--- directory.rb
+++ directory.rb
@@ -50,13 +50,14 @@ def input_students
     cohort = STDIN.gets.chomp
     # adding a default value
     cohort == "" ? cohort = "not decided on " : cohort = cohort.capitalize.intern
-    puts "Please enter hobby (or 'none')"
-    hobby = STDIN.gets.chomp
-    puts "Please enter country of birth"
-    country = STDIN.gets.chomp
-    puts "Please enter height"
-    height = STDIN.gets.chomp
-    @students << {name: name, cohort: cohort, hobby: hobby, country: country, height: height}
+    # puts "Please enter hobby (or 'none')"
+    # hobby = STDIN.gets.chomp
+    # puts "Please enter country of birth"
+    # country = STDIN.gets.chomp
+    # puts "Please enter height"
+    # height = STDIN.gets.chomp
+    # @students << {name: name, cohort: cohort, hobby: hobby, country: country, height: height}
+    build_array(name, cohort)
     # step 8; exercise 9... adding singular/plural "student(s)"
     if @students.count == 1
       puts "Now we have #{@students.count} student"
@@ -79,7 +80,8 @@ def print_students_list
   count = 1
   while count < @students.count do
     @students.each_with_index do |student, index|
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(30), "hobbies: #{student[:hobby]}".center(30), "country of birth: #{student[:country]}".center(30), "height: #{student[:height]}".center(30), ""
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(30)
+      # , "hobbies: #{student[:hobby]}".center(30), "country of birth: #{student[:country]}".center(30), "height: #{student[:height]}".center(30), ""
       count += 1
     end
   end
@@ -91,7 +93,7 @@ end
 
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "w")
+  file = File.open(".gitignore/students.csv", "w")
   #iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -105,14 +107,19 @@ def load_students(filename = ".gitignore/students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    # @students << {name: name, cohort: cohort.to_sym}
+    build_array(name, cohort)
   end
   file.close
 end
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
+  # return if filename.nil? # get out of the method if it isn't given
+  if filename == nil # load default file if none given on start-up (step 14 ex.2)
+    load_students
+    return
+  end
   if File.exist?(filename) # if it exists
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
@@ -120,6 +127,10 @@ def try_load_students
     puts "Sorry, #{filename} doesn't exist."
     exit # quit the program
   end
+end
+
+def build_array(name, cohort)
+  @students <<  {name: name, cohort: cohort.to_sym}
 end
 
 try_load_students
